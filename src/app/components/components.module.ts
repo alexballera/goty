db82ac3865288ff1e1e00d39a37b90566cfa84e7
Alex Ballera <alexballera@gmail.com>
2020-04-22 22:48:52 -0300

    Agregado componentes

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,23 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GraficoBarraHorizontalComponent } from './grafico-barra-horizontal/grafico-barra-horizontal.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    GraficoBarraHorizontalComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    NavbarComponent,
+    GraficoBarraHorizontalComponent,
+  ]
+})
+export class ComponentsModule { }
