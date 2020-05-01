65f9faac3bd6afea357967e96d94d536806e0a5d
Alexander Ballera <alexballera@gmail.com>
2020-04-30 21:26:45 -0300

    Agregada funcionalidad para votar

--- src/app/pages/goty/goty.component.ts
+++ src/app/pages/goty/goty.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from './../../services/game.service';
 import { Game } from './../../interfaces/interfaces';
+import  Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-goty',
@@ -16,9 +17,19 @@ export class GotyComponent implements OnInit {
   ngOnInit() {
     this.gameService.getNominados()
     .subscribe(games => {
-      console.log(games);
       this.juegos = games;
-    })
+    });
+  }
+
+  votarJuego(juego: Game) {
+    this.gameService.votarJuego(juego.id)
+    .subscribe((resp: {ok: boolean, mensaje: string}) => {
+
+      if (resp.ok) {
+        Swal.fire('Gracias', resp.mensaje, 'success');
+      }
+
+    });
   }
 
 }
